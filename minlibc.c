dbbeda1ffd3a883fd1cb885803a266fc54e67f09
Knut Arild Erstad <knutae@gmail.com>
2019-08-12 22:28:12 +0200

    Remove obsolete patching of crt1.o

--- minlibc.c
+++
@@ -1,7 +0,0 @@
-extern int main();
-
-// Minimal implementations of crt1.o libc functions.
-// With these defined, we don't need to link libc. Probably.
-void __libc_csu_init() {}
-void __libc_csu_fini() {}
-void __libc_start_main() { main(); }
