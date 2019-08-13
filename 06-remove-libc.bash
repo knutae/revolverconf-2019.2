86c4b59bcd53b8ac773301d46f3e96d844b32a35
Knut Arild Erstad <knutae@gmail.com>
2019-08-14 00:48:31 +0200

    Remove unused LIBS variable from scripts

--- 06-remove-libc.bash
+++ 06-remove-libc.bash
@@ -20,7 +20,6 @@ mono $SHADER_MINIFIER --preserve-externals fshader.glsl -o gen/shaders.h
 
 CFLAGS="-fomit-frame-pointer -fno-stack-protector -Wall -Werror -Os"
 CFLAGS="$CFLAGS $(pkg-config --cflags gtk+-3.0)"
-LIBS="-lGL $(pkg-config --libs gtk+-3.0)"
 
 # release version
 cc $CFLAGS -DNDEBUG -c 06-solskogen.c -o obj/solskogen-release.o
